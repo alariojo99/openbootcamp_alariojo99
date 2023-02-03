f4bf359c4f889feba57904b7e09696ade980242e
alariojo99 <alejandrolario99@gmail.com>
2023-02-03 15:06:26 +0100

    ejercicio 2

---
+++ Curso JavaScript/02-Sintaxis-Variables-PalabrasReservadas/index.js
@@ -0,0 +1,12 @@
+const persona = {
+    nombre: 'Alejandro',
+    edad: 23,
+    isDeveloper : true,
+    birthdate: new Date(1999, 5, 17),
+    libro : {
+        titulo: 'Geronimo Stilton',
+        autor: 'Elisabetta Dami',
+        fecha: new Date(2010, 0, 1),
+        url: 'https://www.clubgeronimostilton.es/misLibros.php'
+    }
+}
