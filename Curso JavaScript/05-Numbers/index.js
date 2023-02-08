4bb8d4a07a0cbca6e2f0fd9b81e07c97b5800691
alariojo99 <alejandrolario99@gmail.com>
2023-02-08 12:54:16 +0100

    Ejercicio 5

---
+++ Curso JavaScript/05-Numbers/index.js
@@ -0,0 +1,8 @@
+
+const altura = 178
+const alturaM = 1.78
+const peso = 90.5
+const alturaCeil = Math.ceil(alturaM)
+const pesoFloor = Math.floor(peso)
+
+const maxValueMas1 = Number.MAX_VALUE + 1 === Number.MAX_VALUE
